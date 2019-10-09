70f17e8c9261a91ba81731357c0fd2fdbae4ee40
Juscelino <jfilhodev@gmail.com>
2019-10-09 07:42:30 -0300

    atualizacao em 09/10/2019 07:39

--- index.php
+++ index.php
@@ -1,29 +1,33 @@
 <?php
 require_once __DIR__ .'/vendor/autoload.php';
 
-//print_r($_POST);
-// if(isset($_POST['conteudo'])){
-//     $conteudo = json_decode($_POST['conteudo']);
-// }
+
 $json = file_get_contents('php://input');
 $parametros = isset($json) ? json_decode($json) : '';
 
 $conteudo = (isset($parametros->conteudo)? $parametros->conteudo: "");
 $css = 'bootstrap.css';
+//$css = 'estilo.css';
 $time = date("d-m-Y_H:i:s");  ;
 //echo $time;
 
-$arquivo = 'arquivo'.$time.'.pdf';
+$filename = '/relatorios/arquivo'.$time.'.pdf';
+$filepath = __DIR__ .'/relatorios/arquivo'.$time.'.pdf';
+
+
 $mpdf = new \Mpdf\Mpdf();
+
 //$mpdf->WriteHTML('<h1>Hello world!</h1>');
 $mpdf->WriteHTML($css,1);
 $mpdf->WriteHTML($conteudo);
-$mpdf->Output($arquivo,'F');
+$mpdf->Output($filepath,'F');
+// return $mpdf->Output(\Mpdf\Output\Destination::DOWNLOAD);
 
-if(file_exists($arquivo)){
-    $var = file_get_contents($arquivo);
-    return $var;
+if(file_exists($filepath)){
+    echo $filename;
+    // print_r(json_encode(array('status' => '1', 'dados' => 'deu certo')));
 }else{
-    echo 0;
+    print_r(json_encode(array('status' => '0', 'dados' => 'deu ruim')));
 }
-//return ;
+
+//return $$mpdf->Output();;

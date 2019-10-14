16448e659845fee3e55ac52b5daad102f36693ca
Juscelino <jfilhodev@gmail.com>
2019-10-14 07:20:21 -0300

    atualização em 14/10/19 07:20

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ $json = file_get_contents('php://input');
 $parametros = isset($json) ? json_decode($json) : '';
 
 $conteudo = (isset($parametros->conteudo)? $parametros->conteudo: "");
+// echo $conteudo;
+// exit();
 $css = 'bootstrap.css';
-//$css = 'estilo.css';
 $time = date("d-m-Y_H:i:s");  ;
-//echo $time;
 
 $filename = '/relatorios/arquivo'.$time.'.pdf';
 $filepath = __DIR__ .'/relatorios/arquivo'.$time.'.pdf';
@@ -17,15 +17,11 @@ $filepath = __DIR__ .'/relatorios/arquivo'.$time.'.pdf';
 
 $mpdf = new \Mpdf\Mpdf();
 
-//$mpdf->WriteHTML('<h1>Hello world!</h1>');
 $mpdf->WriteHTML($css,1);
 $mpdf->WriteHTML($conteudo);
 $mpdf->Output($filepath,'F');
-// return $mpdf->Output(\Mpdf\Output\Destination::DOWNLOAD);
 
 if(file_exists($filepath)){
-    // echo $filename;
-    // echo (json_encode(array('status' => '1', 'dados' => utf8_encode($filename))));
     echo json_encode(array('status' => '1', 'dados' => $filename));
 }else{
     echo (json_encode(array('status' => '0', 'dados' => 'erro')));

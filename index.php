f401e8c3f7fd61b012a4f50a1638721bc0d88fdf
Juscelino <jfilhodev@gmail.com>
2019-10-30 09:28:14 -0300

    atualizaçao em 30/10/2019 09:28

--- index.php
+++ index.php
@@ -1,28 +1,31 @@
 <?php
-require_once __DIR__ .'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 
 $json = file_get_contents('php://input');
 $parametros = isset($json) ? json_decode($json) : '';
 
-$conteudo = (isset($parametros->conteudo)? $parametros->conteudo: "");
-// echo $conteudo;
-// exit();
-$css = 'bootstrap.css';
-$time = date("d-m-Y_H:i:s");  ;
+$conteudo = (isset($parametros->conteudo) ? $parametros->conteudo : "");
 
-$filename = '/relatorios/arquivo'.$time.'.pdf';
-$filepath = __DIR__ .'/relatorios/arquivo'.$time.'.pdf';
-
-
-$mpdf = new \Mpdf\Mpdf();
-
-$mpdf->WriteHTML($css,1);
-$mpdf->WriteHTML($conteudo);
-$mpdf->Output($filepath,'F');
-
-if(file_exists($filepath)){
-    echo json_encode(array('status' => '1', 'dados' => $filename));
-}else{
+if ($conteudo == "") {
     echo (json_encode(array('status' => '0', 'dados' => 'erro')));
+} else {
+    $css = 'bootstrap.css';
+    $time = date("d-m-Y_H:i:s");;
+
+    $filename = '/relatorios/arquivo' . $time . '.pdf';
+    $filepath = __DIR__ . '/relatorios/arquivo' . $time . '.pdf';
+
+
+    $mpdf = new \Mpdf\Mpdf();
+
+    $mpdf->WriteHTML($css, 1);
+    $mpdf->WriteHTML($conteudo);
+    $mpdf->Output($filepath, 'F');
+
+    if (file_exists($filepath)) {
+        echo json_encode(array('status' => '1', 'dados' => $filename));
+    } else {
+        echo (json_encode(array('status' => '0', 'dados' => 'erro')));
+    }
 }
